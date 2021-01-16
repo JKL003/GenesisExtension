6cbd65fdd5af876a2ae9dd442213fb76542053c6
JKL003 <justinaklam@gmail.com>
2021-01-16 13:14:57 -0500

    Create HTML table using JS

--- ListAssignmentsExtension.js
+++ ListAssignmentsExtension.js
@@ -1,26 +1,40 @@
 removeUnnecessaryHeaders();
 extractGradeData();
-
+showGradeData();
 
 function removeUnnecessaryHeaders() {
      //Removes unnecessary columns
      var headers = document.getElementsByClassName("headerCategoryTab");
      var headersToDelete = ["Attendance", "Grading", "Scheduling", "Documents", "Course Pages"]
      for(var i = headers.length-1; i>-1; i--) {
-         var selectedEl = headers[i];
-         var embeddedText = selectedEl.textContent.trim();
-         if (headersToDelete.includes(embeddedText)) {
-             selectedEl.parentNode.removeChild(selectedEl);
-         }
+          var selectedEl = headers[i];
+          var embeddedText = selectedEl.textContent.trim();
+          if (headersToDelete.includes(embeddedText)) {     
+               try { selectedEl.parentNode.removeChild(selectedEl); }
+               catch (err) { console.log("already deleted those headers"); }
+          }
      }
 
      //Removes student switcher
      var studentHeader = document.getElementsByClassName("headerStudentSelector")[0];
-     studentHeader.parentNode.removeChild(studentHeader);
+     try { studentHeader.parentNode.removeChild(studentHeader); }
+     catch (err) { console.log("already deleted those headers"); }
+     
  
      //Removes the top bar since logout/settings is pointless
      var topHeader = document.getElementsByClassName("headerModules")[0];
-     topHeader.parentNode.removeChild(topHeader);
+     try { topHeader.parentNode.removeChild(topHeader); }
+     catch (err) { console.log("already deleted those headers"); }
+
+}
+
+var assignmentList = [];
+
+function getAssignments() {
+     return assignmentList;
+}
+function setAssignments(newAssignments) {
+     assignmentList = newAssignments;
 }
 
 function extractGradeData() {
@@ -38,7 +52,7 @@ function extractGradeData() {
           // Start new assignment "object"
           if (embeddedText.includes("MP" + mp.toString())) {
                currentAssignmentIndex++;
-               var assignmentInfo = ["Name","Grade","Date"];
+               var assignmentInfo = ["Assignment Name","-----","-----"];
                assignments.push(assignmentInfo);
           }
           // Get all grades
@@ -78,5 +92,50 @@ function extractGradeData() {
      assignments.forEach(function(assignment) {
           console.log(assignment[0] + ":\n\t" + assignment[1] + "\n\t" + assignment[2]);
      });
+     setAssignments(assignments);
+}
+
+function showGradeData() {
+     var assignments = getAssignments();
+     var body = document.getElementsByTagName('body')[0];
+     var allTablesInDoc = document.getElementsByClassName('notecard');
+     var mainTable = allTablesInDoc[allTablesInDoc.length - 1];
+
+     var dataDiv = document.createElement('div');
+     dataDiv.style.width = '90%';
+     dataDiv.style.margin = '50px auto';
+     dataDiv.setAttribute('text-align','center');
      
+     var dataTbl = document.createElement('table');
+     dataTbl.style.width = '100%';
+     dataTbl.setAttribute('border', '2px solid gray');
+     var tbdy = document.createElement('tbody');
+
+     // Create headers
+     var tr = document.createElement('tr');
+     tr.style.backgroundColor = 'rgb(215,215,215)';
+     var tblHeaders = ['Assignment Name','Grade','Date'];
+     for (var i = 0; i < 3; i++) {
+          var td = document.createElement('td');
+          td.style.textAlign = 'center';
+          td.style.fontWeight = 'bold';
+          td.appendChild(document.createTextNode(tblHeaders[i]));
+          tr.appendChild(td);
+     }
+     tbdy.appendChild(tr);
+
+     // Create body of table
+     for (var i = 0; i < assignments.length; i++) {
+          var tr = document.createElement('tr');
+          for (var j = 0; j < 3; j++) {
+               var td = document.createElement('td');
+               j==0 ? td.style.textAlign = 'left' : td.style.textAlign = 'center';
+               td.appendChild(document.createTextNode(assignments[i][j]));
+               tr.appendChild(td);
+          }
+          tbdy.appendChild(tr);
+     }
+     dataTbl.appendChild(tbdy);
+     dataDiv.appendChild(dataTbl);
+     mainTable.appendChild(dataDiv);
 }
